ccb93f4d07deaf4eee4cd51e0a68e4f0905fe0b7
doomsday4 <amankhilani.ajmer@gmail.com>
2025-07-21 04:30:20 +0530

    feat: Disable ESLint during Vercel build

--- my-chat-app/next.config.ts
+++ my-chat-app/next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  eslint: {
+    // ignoring lint warnings during build process, only for vercel production
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
